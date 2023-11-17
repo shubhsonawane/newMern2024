992c5207cf41723ab8a68b129a428ac0c1b7e2e7
unknown <shubhamsonawane76570@gmail.com>
2023-11-17 21:40:41 +0530

    add db connection

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
+require("dotenv").config();
 const express = require("express");
-const router = require("./router/auth-router");
 const app = express();
+const router = require("./router/auth-router");
+const connectDB = require("./utils/db");
 
 app.use("/api/auth", router);
 
 const PORT = 5000;
-app.listen(PORT, () => {
-  console.log(`server is running at port: ${PORT}`);
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`server is running at port: ${PORT}`);
+  });
 });
