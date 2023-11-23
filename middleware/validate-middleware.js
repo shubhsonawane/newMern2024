8fb51facc091d6f5e80b8f93b82afc58471137c5
unknown <shubhamsonawane76570@gmail.com>
2023-11-23 23:06:29 +0530

    add error middleware

--- middleware/validate-middleware.js
+++ middleware/validate-middleware.js
@@ -1,14 +1,19 @@
-const { json } = require("express");
-
 const validate = (schema) => async (req, res, next) => {
   try {
     const parseBody = await schema.parseAsync(req.body);
     req.body = parseBody;
     next();
   } catch (err) {
-    const message = err.errors[0].message;
-    console.log(message);
-    res.status(400).json({ msg: message });
+    const status = 422;
+    const message = "fill the input properly";
+    const extraDetails = err.errors[0].message;
+
+    const error = {
+      status,
+      message,
+      extraDetails
+    };
+    next(error);
   }
 };
 
--- server.js
+++ server.js
@@ -3,9 +3,12 @@ const express = require("express");
 const app = express();
 const router = require("./router/auth-router");
 const connectDB = require("./utils/db");
+const errorMiddleware = require("./middleware/error-middleware");
 app.use(express.json());
 
 app.use("/api/auth", router);
+
+app.use(errorMiddleware);
 
 const PORT = 5000;
 connectDB().then(() => {
